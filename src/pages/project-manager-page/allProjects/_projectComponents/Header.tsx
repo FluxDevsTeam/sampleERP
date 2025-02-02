5f412b081930f9c3fe38552d569d3a0ff2549b6e
Michael Andorful <andorfulmichaelebo@gmail.com>
2025-02-02 17:08:48 +0000

    made cganges to the header

--- src/pages/project-manager-page/allProjects/_projectComponents/Header.tsx
+++ src/pages/project-manager-page/allProjects/_projectComponents/Header.tsx
@@ -8,7 +8,7 @@ export default function Header () {
     <div className="w-auto h-[222px] bg-white  p-8 m-6 ">
         <div className=" space-y-3 ">
         <h1 className="font-bold text-black text-2xl">Manage Artisans</h1>
-      <div className=" hidden md:flex">
+      <div className=" hidden md:flex -space-x-[1.2rem]">
         <img src={userpic}  alt="image"  className="w-[70px] h-[70px] border-4 border-blue-600 rounded-full"  />
         <img src={userpic}  alt="image"  className="w-[70px] h-[70px] border-4 border-blue-600 rounded-full"  />
         <img src={userpic}  alt="image"  className="w-[70px] h-[70px] border-4 border-blue-600 rounded-full"  />
