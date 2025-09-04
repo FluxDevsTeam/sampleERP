6d5b459152ebf3ad69619a2d91abfaa4841c0c4b
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-09-04 12:46:36 +0100

    changed title

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cqcg9safvdo"
+    "revision": "0.5ielu19q2ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
